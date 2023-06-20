458b90694e1bfb0b556dbbb50557b62ffdd4d86a
tbrazier <thomas.brazier@univ-rennes1.fr>
2023-06-20 12:11:28 +0000

    Bugfix in jasmine path to temporary files

--- Scripts/jasmine.sh
+++ Scripts/jasmine.sh
@@ -4,5 +4,5 @@
 sra=$1
 
 # Merging
-jasmine file_list=$sra/${sra}_vcf_list.txt out_file=$sra/${sra}_merged.vcf genome_file=$sra/ref_$sra.fna.gz bam_list=$sra/${sra}_bam_list.txt  --output_genotypes --run_iris
+jasmine file_list=$sra/${sra}_vcf_list.txt out_file=$sra/${sra}_merged.vcf genome_file=$sra/ref_$sra.fna.gz out_dir=$sra/ bam_list=$sra/${sra}_bam_list.txt  --output_genotypes --run_iris
 
--- Scripts/svim.sh
+++ Scripts/svim.sh
@@ -6,4 +6,5 @@ min_sv_size=$2
 min_coverage=$3
 
 # SV calling
-svim alignment $sra $sra/${sra}_sorted.bam $sra/ref_$sra.fna.gz --insertion_sequences --min_sv_size $min_sv_size --minimum_depth $min_coverage > $sra/${sra}_svim.vcf
+svim alignment $sra $sra/${sra}_sorted.bam $sra/ref_$sra.fna.gz --insertion_sequences --min_sv_size $min_sv_size --minimum_depth $min_coverage
+mv ${sra}/variants.vcf $sra/${sra}_svim.vcf
